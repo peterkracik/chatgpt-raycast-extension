59fd3effdd30f14564b75b82c2d88daa93172212
Peter Kracik <kracik@parkside.at>
2023-03-25 11:10:12 +0100

    extensions

--- src/index.tsx
+++ src/index.tsx
@@ -1,91 +1,54 @@
-import { ActionPanel, Action, List } from "@raycast/api";
-import { useFetch, Response } from "@raycast/utils";
-import { useState } from "react";
-import { URLSearchParams } from "node:url";
+import { Detail, getPreferenceValues, LaunchProps } from "@raycast/api";
+import { useEffect, useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
 
-export default function Command() {
-  const [searchText, setSearchText] = useState("");
-  const { data, isLoading } = useFetch(
-    "https://api.npms.io/v2/search?" +
-      // send the search query to the API
-      new URLSearchParams({ q: searchText.length === 0 ? "@raycast/api" : searchText }),
-    {
-      parseResponse: parseFetchResponse,
+
+type Preferences = {
+  openai_api_key: string;
+  openai_model: string;
+  openai_max_tokens: string;
+  openai_temperature: string;
+}
+
+type CommandProps = {
+  query: string
+}
+export default function Command(props: LaunchProps<{ arguments: CommandProps }>) {
+  const { query } = props.arguments;
+  const { openai_api_key, openai_model, openai_max_tokens, openai_temperature } = getPreferenceValues<Preferences>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [result, setResult] = useState<string|null>(null);
+
+  const configuration = new Configuration({
+    apiKey: openai_api_key,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const callPromt = async (prompt: string) => {
+    try {
+      const completion = await openai.createCompletion({
+        model: openai_model,
+        prompt: prompt,
+        n: 1,
+        max_tokens: parseInt(openai_max_tokens),
+        temperature: parseInt(openai_temperature),
+        suffix: "in markdown format",
+      });
+      setResult(completion.data.choices[0].text as string);
+      setIsLoading(false);
+    } catch (error: any) {
+      setIsLoading(false);
+      setResult(error.message);
     }
-  );
-
-  return (
-    <List
-      isLoading={isLoading}
-      onSearchTextChange={setSearchText}
-      searchBarPlaceholder="Search npm packages..."
-      throttle
-    >
-      <List.Section title="Results" subtitle={data?.length + ""}>
-        {data?.map((searchResult) => (
-          <SearchListItem key={searchResult.name} searchResult={searchResult} />
-        ))}
-      </List.Section>
-    </List>
-  );
-}
-
-function SearchListItem({ searchResult }: { searchResult: SearchResult }) {
-  return (
-    <List.Item
-      title={searchResult.name}
-      subtitle={searchResult.description}
-      accessoryTitle={searchResult.username}
-      actions={
-        <ActionPanel>
-          <ActionPanel.Section>
-            <Action.OpenInBrowser title="Open in Browser" url={searchResult.url} />
-          </ActionPanel.Section>
-          <ActionPanel.Section>
-            <Action.CopyToClipboard
-              title="Copy Install Command"
-              content={`npm install ${searchResult.name}`}
-              shortcut={{ modifiers: ["cmd"], key: "." }}
-            />
-          </ActionPanel.Section>
-        </ActionPanel>
-      }
-    />
-  );
-}
-
-/** Parse the response from the fetch query into something we can display */
-async function parseFetchResponse(response: Response) {
-  const json = (await response.json()) as
-    | {
-        results: {
-          package: {
-            name: string;
-            description?: string;
-            publisher?: { username: string };
-            links: { npm: string };
-          };
-        }[];
-      }
-    | { code: string; message: string };
-
-  if (!response.ok || "message" in json) {
-    throw new Error("message" in json ? json.message : response.statusText);
   }
 
-  return json.results.map((result) => {
-    return {
-      name: result.package.name,
-      description: result.package.description,
-      username: result.package.publisher?.username,
-      url: result.package.links.npm,
-    } as SearchResult;
-  });
+  useEffect(() => {
+    callPromt(query);
+  }, [query])
+
+  return <>
+    {result && <Detail markdown={result} />}
+    {isLoading && <Detail markdown="Loading..." />}
+  </>
 }
 
-interface SearchResult {
-  name: string;
-  description?: string;
-  username?: string;
-  url: string;
-}
